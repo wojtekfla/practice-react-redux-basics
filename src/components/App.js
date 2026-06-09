fd2728e911889a7ac11bffb490c96ca28d5fb866
Wojtek <wojtek.wfla@gmail.com>
2026-06-09 21:58:45 +0200

    complete task 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React from "react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-import Task01 from './../../01/Task01';
-import Task02 from './../../02/Task02';
-import Task03 from './../../03/Task03';
-import Task04 from './../../04/Task04';
-import Task05 from './../../05/Task05';
+import Task01 from "./../../01/Task01";
+import Task02 from "./../../02/Task02";
+import Task03 from "./../../03/Task03";
+import Task04 from "./../../04/Task04";
+import Task05 from "./../../05/Task05";
 
+const initialState = {
+  message: "Działa!",
+};
+
+const reducer = (state = initialState) => state;
+
+const store = createStore(reducer);
 
 const App = () => {
-    return (
-        <>
-            <Task01 />
-            {/* <Task02 /> */}
-            {/* <Task03 /> */}
-            {/* <Task04 /> */}
-            {/* <Task05 /> */}
-        </>
-    )
-}
+  return (
+    <Provider store={store}>
+      {/* <Task01 /> */}
+      <Task02 />
+      {/* <Task03 /> */}
+      {/* <Task04 /> */}
+      {/* <Task05 /> */}
+    </Provider>
+  );
+};
 
 export default App;
