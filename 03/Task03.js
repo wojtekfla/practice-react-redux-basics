0f6c30f512e052bb8060c78374c1339b226ab5ce
Wojtek <wojtek.wfla@gmail.com>
2026-06-10 18:17:27 +0200

    complete task 3

--- 03/Task03.js
+++ 03/Task03.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 const Task03 = () => {
-    const time = useSelector(({time = null}) => time);
-    const dispatch = useDispatch();
+  const time = useSelector(({ time = null }) => time);
+  const dispatch = useDispatch();
 
-    return (
-        <section>
-            <h1>Task 3</h1>
-            <div>
-                <time>{ time ? time.toLocaleTimeString() : '00:00:00' }</time>
-                <button onClick={ () => dispatch( {type: 'getCurrentTime'} )}>get current time</button>
-            </div>
-        </section>
-    );
-}
+  return (
+    <section>
+      <h1>Task 3</h1>
+      <div>
+        <time>{time ? time.toLocaleTimeString() : "00:00:00"}</time>
+        <button onClick={() => dispatch({ type: "getCurrentTime" })}>
+          get current time
+        </button>
+      </div>
+    </section>
+  );
+};
 
 export default Task03;
-
--- src/components/App.js
+++ src/components/App.js
@@ -10,18 +10,32 @@ import Task05 from "./../../05/Task05";
 
 const initialState = {
   message: "Działa!",
+  time: new Date(),
 };
 
-const reducer = (state = initialState) => state;
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "getCurrentTime":
+      return {
+        ...state,
+        time: new Date(),
+      };
+    default:
+      return state;
+  }
+};
 
-const store = createStore(reducer);
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(reducer, devTools);
 
 const App = () => {
   return (
     <Provider store={store}>
-      {/* <Task01 /> */}
+      <Task01 />
       <Task02 />
-      {/* <Task03 /> */}
+      <Task03 />
       {/* <Task04 /> */}
       {/* <Task05 /> */}
     </Provider>
